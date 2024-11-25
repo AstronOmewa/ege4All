6731ec4368c753b324140f4ecd24592420d67994
AstrOmewa <omewa242@gmail.com>
2024-11-25 09:39:26 +0300

    modified:   practice/12hw.py 	modified:   practice/17hw.py 	new file:   practice/data_17hw/17.txt 	new file:   practice/data_17hw/17_1.txt 	new file:   practice/data_17hw/17_2.txt 	new file:   progress_check/25-11-24/17_1.txt 	new file:   progress_check/25-11-24/17_2.txt 	new file:   progress_check/25-11-24/answers/17_1A.txt 	new file:   progress_check/25-11-24/answers/17_2A.txt 	new file:   progress_check/25-11-24/solutions/17_1.py 	new file:   progress_check/25-11-24/solutions/17_2.py 	new file:   progress_check/25-11-24/Тест 25-11-24.docx

--- practice/12hw.py
+++ practice/12hw.py
@@ -86,7 +86,7 @@
 s = '8'*82
 
 while '1111' in s or '8888' in s:
-    if '1111' in s: s = s.replace('1111','8')
-    else: s = s.replace('8888','11')
+    if '1111' in s: s = s.replace('1111','8',1)
+    else: s = s.replace('8888','11',1)
 
 print(s)
